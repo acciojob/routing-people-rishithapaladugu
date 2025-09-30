52bd2101a02c4075a9411dcfe0d364200482e682
rishithapaladugu <116055808+rishithapaladugu@users.noreply.github.com>
2025-09-30 09:50:02 +0530

    Create UserDetails.js

---
+++ src/components/UserDetails.js
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+const UserDetails = () => {
+  const { id } = useParams();
+  const [user, setUser] = useState(null);
+  const [loading, setLoading] = useState(true);
+
+useEffect(() => {
+    setLoading(true);
+    setTimeout(() => {
+      fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+        .then((res) => res.json())
+        .then((data) => {
+          setUser(data);
+          setLoading(false);
+        });
+    }, 500); 
+  }, [id]);
+
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!user) return null;
+
+  return (
+    <div>
+        
+      <p>Name: {user.name}</p>
+      <p>Username: {user.username}</p>
+      <p>Email: {user.email}</p>
+      <p>Phone: {user.phone}</p>
+      <p>Website: {user.website}</p>
+    </div>
+  );
+};
+
+export default UserDetails;
