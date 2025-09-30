1fe04b5ae33412ff4f002bdc33571072e1381092
rishithapaladugu <116055808+rishithapaladugu@users.noreply.github.com>
2025-09-30 10:29:56 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,35 +1,17 @@
-import React, { useEffect, useState } from "react";
-import "./../styles/App.css";
+import React from "react";
+import { Route, Switch } from "react-router-dom";
 import UserList from "./UserList";
 import UserDetails from "./UserDetails";
-import { Routes, Route } from "react-router-dom";
-import axios from "axios";
-import "regenerator-runtime/runtime";
 
-const App = () => {
-  const [users, setUsers] = useState([]);
-
-  const fetchUsers = async () => {
-    try {
-      const response = await axios.get("https://jsonplaceholder.typicode.com/users/");
-      setUsers(response.data)
-    } catch (err) {
-      console.error(err);
-    }
-  };
-
-  useEffect(() => {
-    fetchUsers();
-  }, []);
-
+function App() {
   return (
     <div>
-      <Routes>
-        <Route path="/" element={<UserList users={users} />} />
-        <Route path="/users/:id" element={<UserDetails />} />
-      </Routes>
+      <Switch>
+        <Route exact path="/" component={UserList} />
+        <Route path="/users/:id" component={UserDetails} />
+      </Switch>
     </div>
   );
-};
+}
 
 export default App;
