9f9947d09e9120a637a04de3e092f58a5b654fcd
rishithapaladugu <116055808+rishithapaladugu@users.noreply.github.com>
2025-09-30 10:26:34 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter } from "react-router-dom";  // ✅ IMPORT THIS
 import App from "./components/App";
-import {BrowserRouter} from 'react-router-dom';
 
-
-ReactDOM.render(<BrowserRouter><App /></BrowserRouter>, document.getElementById("root"));
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root")
+);
