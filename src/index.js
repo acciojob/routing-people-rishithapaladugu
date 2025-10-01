bc311f2d22492c9435e6884199e531fb12eac391
rishithapaladugu <116055808+rishithapaladugu@users.noreply.github.com>
2025-10-01 10:05:23 +0530

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,11 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import { BrowserRouter } from "react-router-dom";  // ✅ IMPORT THIS
 import App from "./components/App";
 
-ReactDOM.render(
-  <BrowserRouter>
-    <App />
-  </BrowserRouter>,
-  document.getElementById("root")
-);
+
+
+ReactDOM.render(<App />, document.getElementById("root"));
