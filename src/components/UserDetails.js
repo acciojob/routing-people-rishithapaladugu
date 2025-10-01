7df6bf5ea5456eb9bc4de483e5f77a5734b61384
rishithapaladugu <116055808+rishithapaladugu@users.noreply.github.com>
2025-10-01 09:55:52 +0530

    Update UserDetails.js

--- src/components/UserDetails.js
+++ src/components/UserDetails.js
@@ -1,34 +1,39 @@
 import React, { useEffect, useState } from "react";
-import { useParams, Link } from "react-router-dom";
+import { useParams } from "react-router-dom";
 
-function UserDetails() {
+const UserDetails = () => {
   const { id } = useParams();
   const [user, setUser] = useState(null);
   const [loading, setLoading] = useState(true);
 
-  useEffect(() => {
+useEffect(() => {
     setLoading(true);
-    fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
-      .then((res) => res.json())
-      .then((data) => {
-        setUser(data);
-        setLoading(false);
-      });
+    setTimeout(() => {
+      fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+        .then((res) => res.json())
+        .then((data) => {
+          setUser(data);
+          setLoading(false);
+        });
+    }, 500); 
   }, [id]);
 
-  if (loading) return <div>Loading...</div>;
+  if (loading) {
+    return <div>Loading...</div>;
+  }
+
+  if (!user) return null;
 
   return (
     <div>
+        
       <p>Name: {user.name}</p>
       <p>Username: {user.username}</p>
       <p>Email: {user.email}</p>
       <p>Phone: {user.phone}</p>
       <p>Website: {user.website}</p>
-
-      <Link to="/">Back to User List</Link>
     </div>
   );
-}
+};
 
 export default UserDetails;
